3640f8a26a097ff728a0917614c74ceaeb5985b8
Storm-4 <storm-4@Admin-4s-iMac.local>
2023-07-06 13:15:16 +0800

    add day08 protject

--- day_08/lib/day_08.dart
+++ day_08/lib/day_08.dart
@@ -1,3 +1,28 @@
 int calculate() {
   return 6 * 7;
 }
+class humanRace {
+  int eyes = 0;
+}
+class Rectangle{
+  int _width= 0;
+  int _height= 0;
+  void getMeasures(){
+    print('im a rectangle. i have a height of $_height and width of $_width');
+  }
+  void setWidth(int width){
+    this._width=width;
+
+  }
+  void setHeight(int height){
+    this._height = height;
+  }
+}
+void main(){
+final rectangle = Rectangle();
+print (rectangle);
+rectangle.getMeasures();
+rectangle.setWidth(5);
+rectangle.setHeight(10);
+rectangle.getMeasures();
+}
