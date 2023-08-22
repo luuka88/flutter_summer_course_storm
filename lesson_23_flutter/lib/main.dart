46b917fb4be2cf2b64900f8956a2a32bd3e2404e
Storm-4 <storm-4@Admin-4s-iMac.local>
2023-08-22 12:37:18 +0800

    lesson22

---
+++ lesson_23_flutter/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:lesson_23_flutter/screen_layout.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: ScreenLayout(),
+  ));
+}
+
+
---
+++ lesson_23_flutter/lib/screens/add_post_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddPostScreen extends StatelessWidget {
+  const AddPostScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Container(
+        child: SafeArea(child: Center(child: Text('AddPostScreen'),),),
+      ),
+    );
+  }
+}
