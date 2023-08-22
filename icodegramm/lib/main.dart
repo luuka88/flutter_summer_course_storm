56f7c307d5be58dd3471d18c9ab7c092792048ca
Storm-4 <storm-4@Admin-4s-iMac.local>
2023-08-22 13:00:03 +0800

    icodegramm

---
+++ icodegramm/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:lesson_20_flutter/screens/login_screen.dart';
+import 'package:lesson_20_flutter/screens/signup_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MaterialApp(home: LoginScreen()));
